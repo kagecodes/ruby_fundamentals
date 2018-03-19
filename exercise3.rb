7a30e73cc18f0b294644e57a0f76b92b7a4716cd
Karen <kjgamis@live.ca>
2018-03-19 14:38:50 -0400

    add user age and year

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,9 @@ puts "What is your name?"
 user_name = gets.chomp
 
 puts "Hello, #{user_name}"
+
+
+puts "What is your age?"
+user_age = gets.chomp
+
+puts "You were born on either #{2017 - user_age.to_i} or #{2018 - user_age.to_i}"

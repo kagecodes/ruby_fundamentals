024dc7f80d6d5f1488aededce2be354cf6b50f2d
Karen <kjgamis@live.ca>
2018-03-19 14:11:09 -0400

    answer exercise 2 questions

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+# Part 1
+meal = 55
+
+puts meal*0.15
+
+
+# Part 2
+
+puts "the cost of the apple is $" + 1.to_s
+
+
+# Part 3
+
+puts "45 628 multiplied by 7839 is #{45628*7839}"
+
+
+# Part 4
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)

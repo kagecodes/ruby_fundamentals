06309ca5eafbdf8a68857cdba52469ac7ebe74d3
Karen <kjgamis@live.ca>
2018-03-19 13:55:47 -0400

    add escape sequences and make exercise 2 file

--- exercise1.rb
+++ exercise1.rb
@@ -1,1 +1,10 @@
-puts 2 puts 3 puts 2 + 3
+# puts 2
+# puts 3
+# puts 2 + 3
+
+puts "Betty's pie shop"
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
